fd6cc62ec9de7da86e664b42c71e7799979dacb1
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-07-08 11:29:32 -0300

    🔨 adds development scripts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,35 @@
 import Hapi from '@hapi/hapi'
+import Vision from '@hapi/vision'
+import path from 'path'
+import Pug from 'pug'
 
-export default function init() {
+export default async function init() {
   const server = Hapi.server({
     port: process.env.PORT || 4000,
     host: '0.0.0.0',
   })
 
+  await server.register(Vision)
+
+  server.views({
+    engines: { pug: Pug },
+    relativeTo: __dirname,
+    path: 'views',
+    compileOptions: {
+      // By default Pug uses relative paths (e.g. ../root.pug), when using absolute paths (e.g. include /root.pug), basedir is prepended.
+      // https://pugjs.org/language/includes.html
+      basedir: path.join(__dirname, 'views'),
+    },
+  })
+
   server.route({
     method: 'GET',
     path: '/',
-    handler: () => 'Hello World!',
+    handler: (request, h) =>
+      h.view('index', {
+        title: `Hapi - ${request.server.version}`,
+        message: 'Hello Pug!',
+      }),
   })
 
   server.start()
